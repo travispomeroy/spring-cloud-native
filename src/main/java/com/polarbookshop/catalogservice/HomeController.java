c55ca1615d5425627a819192459bba559f1831d0
travispomeroy <blah@blah.com>
2024-12-08 09:11:45 -0500

    Chapter 4.

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,14 +1,21 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-	@GetMapping("/")
+	private final PolarProperties polarProperties;
+
+  public HomeController(PolarProperties polarProperties) {
+    this.polarProperties = polarProperties;
+  }
+
+  @GetMapping("/")
 	public String home() {
-		return "Welcome to the Book Catalog Service!";
+		return this.polarProperties.getGreeting();
 	}
 
 }
